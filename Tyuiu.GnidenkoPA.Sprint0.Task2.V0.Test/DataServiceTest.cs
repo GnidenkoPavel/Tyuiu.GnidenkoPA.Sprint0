d14ae3a7f39ecf95383ee9fedefed345f612da78
gnide <gnide@DESKTOP-AE6RANS>
2025-09-10 21:21:46 +0500

    Task2

---
+++ Tyuiu.GnidenkoPA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.GnidenkoPA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.GnidenkoPA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMassageValid()
+        {
+            var name = "Паша";
+            var res = DataService.GetMassage(name);
+            Assert.AreEqual("Привет, Паша", res);
+        }
+    }
+}
---
+++ Tyuiu.GnidenkoPA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Tyuiu.GnidenkoPA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.GnidenkoPA.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMassage("Паша"));
+            Console.ReadKey();
+        }
+    }
+}
+
