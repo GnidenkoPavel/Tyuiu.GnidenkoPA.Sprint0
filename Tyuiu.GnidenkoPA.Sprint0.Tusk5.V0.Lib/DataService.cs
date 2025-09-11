729ba9e12157432d25266356d74a612b5a5ef39f
gnide <gnide@DESKTOP-AE6RANS>
2025-09-11 22:28:40 +0500

    Task5

---
+++ Tyuiu.GnidenkoPA.Sprint0.Tusk5.V0.Lib/DataService.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.Design;
+
+namespace Tyuiu.GnidenkoPA.Sprint0.Tusk5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+
+        {
+
+            return a + b;
+        }
+        public static int Substraction(int a, int b)
+
+        {
+
+            return a - b;
+        }
+        public static int Multiplication(int a, int b)
+
+        {
+
+            return a * b;
+        }
+
+        public static double Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Переменная b = {0} на ноль делить нельзя", b);
+                return -1;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+            
+    }
+}
